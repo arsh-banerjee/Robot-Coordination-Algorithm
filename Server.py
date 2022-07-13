f73516e3df701fdc47410a6323ca2d19059ab9c7
arsh-banerjee <78701372+arsh-banerjee@users.noreply.github.com>
2022-07-13 12:10:22 -0700

    Add files via upload

--- Server.py
+++ Server.py
@@ -1,17 +1,33 @@
-import socket
+import sys
+import pyzed.sl as sl
+import cv2
 
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = "192.168.1.180"
-port = 30000
-print (host)
-print (port)
-serversocket.bind((host, port))
+init = sl.InitParameters()
+init.camera_resolution = sl.RESOLUTION.HD720
+init.depth_mode = sl.DEPTH_MODE.PERFORMANCE
 
-serversocket.listen(5)
-print ('server started and listening')
 
-(clientsocket, address) = serversocket.accept()
+ip = 'localhost'
+init.set_from_stream(ip)
 
-while True:
-    data = clientsocket.recv(1024).decode()
-    print (data)
+cam = sl.Camera()
+status = cam.open(init)
+if status != sl.ERROR_CODE.SUCCESS:
+    print(repr(status))
+    exit(1)
+
+runtime = sl.RuntimeParameters()
+mat = sl.Mat()
+
+key = ''
+print("  Quit : CTRL+C\n")
+while key != 113:
+    err = cam.grab(runtime)
+    if (err == sl.ERROR_CODE.SUCCESS) :
+        cam.retrieve_image(mat, sl.VIEW.LEFT)
+        cv2.imshow("ZED", mat.get_data())
+        key = cv2.waitKey(1)
+    else :
+        key = cv2.waitKey(1)
+
+cam.close()
