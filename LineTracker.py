64fbda09cec8553193590b5f7b156bd3017be351
arsh-banerjee <78701372+arsh-banerjee@users.noreply.github.com>
2022-07-14 10:03:31 -0700

    Add files via upload

--- LineTracker.py
+++ LineTracker.py
@@ -1,36 +1,78 @@
 import cv2
 import roslibpy
 import time
-# import the necessary packages
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-import time
-import cv2
-# initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-rawCapture = PiRGBArray(camera)
-# allow the camera to warmup
-time.sleep(0.1)
-# grab an image from the camera
+import numpy as np
 
+# define a video capture object
+vid = cv2.VideoCapture(4)
 client = roslibpy.Ros(host='192.168.1.180', port=9090)
 client.run()
-cmd_topic = roslibpy.Topic(client, '/joy_vel_0', 'geometry_msgs/Twist', throttle_rate=50)
+cmd_topic = roslibpy.Topic(client, '/cmd_vel', 'geometry_msgs/Twist', throttle_rate=50)
 cmd_topic.advertise()
 
-while True:
-    camera.capture(rawCapture, format="bgr")
-    image = rawCapture.array
-    print(image)
-    if False:
-        linear_speed = 0.05
-        angular_speed = 0
+angular_speed = 0
+linear_speed = 0.05
 
-    msg = roslibpy.Message({
-        'linear': {'x': linear_speed, 'y': 0.0, 'z': 0.0},
-        'angular': {'x': 0.0, 'y': 0.0, 'z': 0.0}
-    })
+left_pixel = (300,50)
+right_pixel = (300,590)
+
+while (True):
+    # Capture the video frame
+    # by frame
+    ret, frame = vid.read()
+    center_pixel = (round(frame.shape[0] / 2), round(frame.shape[1] / 2))
+    center_pixel_rgb = frame[center_pixel[0],center_pixel[1],:]
+    left_pixel_rgb = frame[left_pixel[0],left_pixel[1],:]
+    right_pixel_rgb = frame[right_pixel[0], right_pixel[1], :]
+
+    if np.min(right_pixel_rgb) > 150:
+        while(np.min(right_pixel_rgb) > 150):
+            angular_speed -= 0.01
+            msg = roslibpy.Message({'linear': {'x': linear_speed, 'y': 0.0, 'z': 0.0},
+                                    'angular': {'x': 0.0, 'y': 0.0, 'z': angular_speed}})
+            cmd_topic.publish(msg)
+            ret, frame = vid.read()
+            right_pixel_rgb = frame[right_pixel[0], right_pixel[1], :]
+        angular_speed = 0
+        msg = roslibpy.Message({'linear': {'x': linear_speed, 'y': 0.0, 'z': 0.0},
+                                'angular': {'x': 0.0, 'y': 0.0, 'z': angular_speed}})
+        cmd_topic.publish(msg)
+
+    if np.min(left_pixel_rgb) > 150:
+        while(np.min(left_pixel_rgb) > 150):
+            angular_speed += 0.01
+            msg = roslibpy.Message({'linear': {'x': linear_speed, 'y': 0.0, 'z': 0.0},
+                                    'angular': {'x': 0.0, 'y': 0.0, 'z': angular_speed}})
+            cmd_topic.publish(msg)
+            ret, frame = vid.read()
+            left_pixel_rgb = frame[left_pixel[0], left_pixel[1], :]
+        angular_speed = 0
+        msg = roslibpy.Message({'linear': {'x': linear_speed, 'y': 0.0, 'z': 0.0},
+                                'angular': {'x': 0.0, 'y': 0.0, 'z': angular_speed}})
+        cmd_topic.publish(msg)
+
+    msg = roslibpy.Message({ 'linear': {'x': linear_speed, 'y': 0.0, 'z': 0.0},
+        'angular': {'x': 0.0, 'y': 0.0, 'z': angular_speed}})
 
     cmd_topic.publish(msg)
 
-    client.terminate()
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+
+    # the 'q' button is set as the
+    # quitting button you may use any
+    # desired button of your choice
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
+client.terminate()
+
+
+
+
+
+
